2220e1a9c636dfb43ca961873613a7bdc5885b95
Keely Brennan <keelybrennan4@gmail.com>
2020-03-11 10:26:09 -0500

    userio

--- TeaCupInventory/src/main/java/io/UserIOConsoleImpl.java
+++ TeaCupInventory/src/main/java/io/UserIOConsoleImpl.java
@@ -9,6 +9,56 @@ package io;
  *
  * @author keelybrennan
  */
-public class UserIOConsoleImpl {
+public class UserIOConsoleImpl implements UserIO {
+
+    @Override
+    public void print(String msg) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double readDouble(String prompt) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double readDouble(String prompt, double min, double max) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public float readFloat(String prompt) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public float readFloat(String prompt, float min, float max) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int readInt(String prompt) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int readInt(String prompt, int min, int max) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public long readLong(String prompt) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public long readLong(String prompt, long min, long max) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String readString(String prompt) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
