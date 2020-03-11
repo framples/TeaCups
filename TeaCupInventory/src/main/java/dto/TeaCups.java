626a933382648d969f18e55703fa7de75283ac4b
Keely Brennan <keelybrennan4@gmail.com>
2020-03-11 10:39:47 -0500

    dto

--- TeaCupInventory/src/main/java/dto/TeaCups.java
+++ TeaCupInventory/src/main/java/dto/TeaCups.java
@@ -5,10 +5,59 @@
  */
 package dto;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 /**
  *
  * @author keelybrennan
  */
 public class TeaCups {
-    
+    private int manufacturer;
+    private String name; 
+    private String color;
+    private BigDecimal price;
+    private LocalDate timeAcquired;
+
+    public int getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(int manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public LocalDate getTimeAcquired() {
+        return timeAcquired;
+    }
+
+    public void setTimeAcquired(LocalDate timeAcquired) {
+        this.timeAcquired = timeAcquired;
+    }
+
+
 }
