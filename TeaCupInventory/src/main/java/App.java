a964fe413a8858fd148543becdc606a122851840
Illio, the Great <pwfcoding@gmail.com>
2020-03-12 13:23:09 -0500

    changed over to spring - pom, xml and app all updated

--- TeaCupInventory/src/main/java/App.java
+++ TeaCupInventory/src/main/java/App.java
@@ -5,6 +5,8 @@ import dao.TeaCupDaoFileImpl;
 import io.TeaCupView;
 import io.UserIO;
 import io.UserIOConsoleImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -19,11 +21,14 @@ import io.UserIOConsoleImpl;
 public class App {
     public static void main(String[] args) {
         
-        UserIO io = new UserIOConsoleImpl();
+      /*  UserIO io = new UserIOConsoleImpl();
         
         TeaCupDao dao = new TeaCupDaoFileImpl();
         TeaCupView view = new TeaCupView(io);
-        TeaCupController controller = new TeaCupController(dao, view);
+        TeaCupController controller = new TeaCupController(dao, view); */
+        
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+        TeaCupController controller = ctx.getBean("controller", TeaCupController.class);
 
         controller.run();
     
